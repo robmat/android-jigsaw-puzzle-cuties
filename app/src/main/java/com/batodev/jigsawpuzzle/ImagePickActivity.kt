a2d0b110d7361296a95714d29b6793abdf864499
Robert Batorowski <Robert.Batorowski@redwood.com>
2024-02-09 14:16:45 +0100

    feat: proper use of permissions and requesting for images

--- app/src/main/java/com/batodev/jigsawpuzzle/ImagePickActivity.kt
+++ app/src/main/java/com/batodev/jigsawpuzzle/ImagePickActivity.kt
@@ -1,12 +1,13 @@
 package com.batodev.jigsawpuzzle
 
 import android.Manifest
-import android.content.ContentValues
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
@@ -21,9 +22,12 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -32,16 +36,17 @@ import java.io.IOException
 private const val CUSTOM = "Custom"
 private const val HARD = "Hard"
 private const val MEDIUM = "Medium"
-private const val CAMERA_REQUEST = 1888
 
+
+private const val CAMERA_PERMISSION_REQUEST_CODE = 1
+private const val EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 2
 
 class ImagePickActivity : AppCompatActivity() {
-    private var imageUri: Uri? = null
     private var files: Array<String> = arrayOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.image_pick_activity)
-        supportActionBar?.hide();
+        supportActionBar?.hide()
         val am = assets
         try {
             files = am.list("img") ?: arrayOf()
@@ -132,10 +137,11 @@ class ImagePickActivity : AppCompatActivity() {
                 alertDialog
             )
         }
-        popupView.findViewById<CheckBox>(R.id.background_image_checkbox).setOnCheckedChangeListener { _, value ->
-            settings.showImageInBackgroundOfThePuzzle = value
-            SettingsHelper.save(this, settings)
-        }
+        popupView.findViewById<CheckBox>(R.id.background_image_checkbox)
+            .setOnCheckedChangeListener { _, value ->
+                settings.showImageInBackgroundOfThePuzzle = value
+                SettingsHelper.save(this, settings)
+            }
     }
 
     private fun startTheGame(
@@ -226,49 +232,103 @@ class ImagePickActivity : AppCompatActivity() {
 
     private val cameraActivityResultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ) {
-        imageUri?.let {
-            copyFileAndStartGame(it)
-        }
-    };
-
-    private fun copyFileAndStartGame(it: Uri) {
-       contentResolver.openFileDescriptor(it, "r").use { parcelFileDescriptor ->
-            val fileDescriptor = parcelFileDescriptor!!.fileDescriptor
-            val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
-            val pathToSave = File(filesDir.absolutePath, "temp.jpg")
-            FileOutputStream(pathToSave).use {
-                image.compress(Bitmap.CompressFormat.JPEG, 90, it)
+    ) { ar ->
+        if (ar.resultCode == Activity.RESULT_OK) {
+            val resultIntent = ar.data
+            resultIntent?.let {
+                val extras = it.extras
+                extras?.let {
+                    val photoAny: Any? = extras.get("data")
+                    if (photoAny is Bitmap) {
+                        val photoBitmap = photoAny as Bitmap
+                        photoBitmap.let { bm ->
+                            val pathToSave = File(filesDir, "temp.jpg")
+                            FileOutputStream(pathToSave).use { fos ->
+                                bm.compress(Bitmap.CompressFormat.JPEG, 90, fos)
+                            }
+                            showStartGamePopup(null, pathToSave.toString())
+                        }
+                    }
+                }
             }
-            showStartGamePopup(null, pathToSave.toString())
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraActivityResultLauncher.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+            } else {
+                Toast.makeText(this, "Camera permission denied.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImageFromGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            } else {
+                Toast.makeText(this, "Exetrnal storage permission denied.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     fun onImageFromCameraClick(view: View?) {
-        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
         ) {
-            val permission =
-                arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            requestPermissions(permission, 112)
+            requestPermissions(
+                listOf(Manifest.permission.CAMERA).toTypedArray(),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            cameraActivityResultLauncher.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
         }
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE, "New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-        cameraActivityResultLauncher.launch(cameraIntent)
     }
 
-    private var pickImageFromGallery = registerForActivityResult<String, Uri>(
-        ActivityResultContracts.GetContent()
+    private fun copyFileAndStartGame(it: Uri?) {
+        it?.let {
+            contentResolver.openFileDescriptor(it, "r").use { parcelFileDescriptor ->
+                val fileDescriptor = parcelFileDescriptor!!.fileDescriptor
+                val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                val pathToSave = File(filesDir.absolutePath, "temp.jpg")
+                FileOutputStream(pathToSave).use {
+                    image.compress(Bitmap.CompressFormat.JPEG, 90, it)
+                }
+                showStartGamePopup(null, pathToSave.toString())
+            }
+        }
+    }
+
+    private var pickImageFromGallery = registerForActivityResult<PickVisualMediaRequest, Uri>(
+        ActivityResultContracts.PickVisualMedia()
     ) {
         copyFileAndStartGame(it)
     }
 
     fun onImageFromGalleryClick(view: View?) {
-        pickImageFromGallery.launch("image/*")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            askForReadExternalImagesPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        } else {
+            askForReadExternalImagesPermission(Manifest.permission.READ_MEDIA_IMAGES)
+        }
+    }
+
+    private fun askForReadExternalImagesPermission(readExternalStorage: String) {
+        if (checkSelfPermission(
+                readExternalStorage
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(readExternalStorage),
+                EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            pickImageFromGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
     }
 
     companion object {
